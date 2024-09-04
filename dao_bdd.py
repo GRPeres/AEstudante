931f7b51eeb877faba19637d4827d6775a7c067f
Gabriel Ribeiro Peres <gr.peres@hotmail.com>
2024-09-04 19:54:08 -0300

    Initial commit

---
+++ dao_bdd.py
@@ -0,0 +1,67 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import insert
+
+class DAO:
+    name = "root"
+    password = "root"
+    host = "localhost"
+    database = "aestudante_db"
+    
+    def __init__(self):
+        # Connect to the database schema
+        engine = create_engine("mysql+mysqlconnector://" + self.name + ":" + self.password + "@" + self.host + "/" + self.database + "?charset=utf8mb4")
+
+        # ORM mapping with SQLAlchemy
+        db = automap_base()
+        db.prepare(autoload_with=engine)
+        self.produto = db.classes.tb_produtos
+        
+        self.idt = "id_produtos"
+        self.nme = "nome_produtos"
+        self.des = "descricao_produtos"
+        self.qtd = "quantidade_produtos"
+
+        # Work with ORM sessions
+        session_factory = sessionmaker(bind=engine)
+        self.ses = session_factory()
+
+    def create(self, obj):
+        self.ses.add(obj)
+        self.ses.commit()
+
+    def readAll(self):
+        return self.ses.query(self.produto).all()
+
+    def readById(self, id):
+        return self.ses.query(self.produto).filter(self.produto.id_produtos == id).first()
+
+    def readByNome(self, nome):
+        return self.ses.query(self.produto).filter(self.produto.nome_produtos.ilike(f'%{nome}%')).all()
+
+    def readByNomeFirstMatch(self, nome): 
+        return self.ses.query(self.produto).filter(self.produto.nome_produtos.ilike(f'%{nome}%')).first()
+    def readBy(self, campo, oper, valor):
+        if oper == "==":
+            query = getattr(self.produto, campo) == valor
+        elif oper == "ilike":
+            query = getattr(self.produto, campo).ilike(f'%{valor}%')
+        else:
+            query = eval(f"self.produto.{campo} {oper} valor")
+
+        return self.ses.query(self.produto).filter(query).all()
+
+    def update(self, obj):
+        self.ses.merge(obj)
+        self.ses.commit()
+
+    def delete(self, obj):
+        self.ses.delete(obj)
+        self.ses.commit()
+
+    def getSes(self):
+        return self.ses
+
+    def __del__(self):
+        self.ses.close()
---
+++ main.py
@@ -0,0 +1,80 @@
+from flask import Flask, request, jsonify, render_template, url_for, redirect
+from flask_login import current_user, LoginManager, login_user, logout_user, login_required, UserMixin
+from dao_bdd import DAO
+
+app = Flask(__name__)
+app.secret_key = 'test1'
+
+#Rota principal da pagina inicial
+@app.route('/', methods=['GET'])
+def index(): 
+    return render_template('index.html')
+
+#Rota principal da pagina de controle de estoque
+@app.route('/adm/', methods=['GET'])
+def adm(): 
+    dao = DAO()
+    produtos = dao.readAll()
+    return render_template('adm.html', produtos=produtos)
+
+#rota auxiliar que atualiza dados na tabela produto
+@app.route('/<int:id>/update', methods=['POST'])
+def update_produto(id):
+    dao = DAO()
+    
+    nome_produtos = request.form['nome_produtos']
+    descricao_produtos = request.form['descricao_produtos']
+    quantidade_produtos = request.form['quantidade_produtos']
+    
+    produto = dao.readById(id)
+    if produto:
+        produto.nome_produtos = nome_produtos
+        produto.descricao_produtos = descricao_produtos
+        produto.quantidade_produtos = quantidade_produtos
+        
+        dao.update(produto)
+        return ('', 204)
+    return ('Product not found', 404)
+
+#rota auxiliar que cria um  novo produto na tabela
+@app.route('/add',  methods=['POST'])
+def add_product():
+    dao = DAO()
+
+    novo_produto = dao.produto(
+        nome_produtos= 'filler',
+        descricao_produtos= 'filler',
+        quantidade_produtos= '0'
+    )
+
+    try:
+        dao.create(novo_produto)
+        return jsonify({
+            'message': 'Product added successfully',
+            'id': novo_produto.id_produtos
+        }), 201
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        return jsonify({'error': str(e)}), 400
+    
+#rota auxiliar que deleta um produto da tabela
+@app.route('/<int:id>/delete', methods=['POST'])
+def delete_produto(id):
+    dao = DAO()
+    produto = dao.readById(id)
+    if produto:
+        dao.delete(produto)
+        return ('', 204)
+    return ('Product not found', 404)
+
+@app.route('/filter', methods=['POST'])
+def filter_products():
+    nome_produtos = request.form.get('nome_produtos')
+    descricao_produtos = request.form.get('descricao_produtos')
+    quantidade_produtos = request.form.get('quantidade_produtos')
+
+    # Implement your filtering logic here, e.g., querying the database
+
+    return jsonify({'produtos': 'null'})
+
+app.run(debug=True)
