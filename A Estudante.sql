a6c77a935efb75dba507a79bec575cf517ad0d73
Gabriel Mousinho Daia <126831870+GabrielMousinho@users.noreply.github.com>
2024-09-14 13:17:58 -0300

    Add files via upload

---
+++ A Estudante.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE aestudante_db;
+
+USE aestudante_db;
+
+CREATE TABLE tb_usuario(          
+	id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+	nome_usuario VARCHAR(50) NOT NULL,
+	email_usuario VARCHAR(50) NOT NULL,
+    senha_usuario VARCHAR(50) NOT NULL,
+	endereço_usuario VARCHAR(50),
+	cep_usuario INT
+	);
+
+CREATE TABLE tb_produtos(          
+	id_produtos INT AUTO_INCREMENT PRIMARY KEY,
+	nome_produtos VARCHAR(50) NOT NULL,
+	descricao_produtos VARCHAR(250) NOT NULL,
+    quantidade_produtos INT NOT NULL
+	);
