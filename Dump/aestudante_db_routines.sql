0ce40ed8290259e05612bf84aa0b3d525fe5c699
Gabriel Mousinho Daia <126831870+GabrielMousinho@users.noreply.github.com>
2024-11-14 09:59:32 -0300

    Add files via upload

--- Dump/aestudante_db_routines.sql
+++ Dump/aestudante_db_routines.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `aestudante_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `aestudante_db`;
 -- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
 --
 -- Host: localhost    Database: aestudante_db
@@ -32,4 +34,4 @@
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-10-30 15:35:19
+-- Dump completed on 2024-11-14  9:57:39
--- Dump/aestudante_db_tb_usuario.sql
+++ Dump/aestudante_db_tb_usuario.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `aestudante_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `aestudante_db`;
 -- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
 --
 -- Host: localhost    Database: aestudante_db
@@ -51,4 +53,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-10-30 15:35:18
+-- Dump completed on 2024-11-14  9:57:39
